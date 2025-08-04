7c5edff6f28995a061dc31ed0ac5a8db4d351368
Amir Hurtado <a.hurtado@utp.edu.co>
2025-08-03 20:52:36 -0500

    feat: enhance CommentReplies component; add toggle functionality for viewing responses and update button rendering

--- client/src/components/Feed/Post/Comments/CommentReplies/CommentReplies.tsx
+++ client/src/components/Feed/Post/Comments/CommentReplies/CommentReplies.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { ChevronDown } from "lucide-react";
+import { ChevronDown, ChevronUp } from "lucide-react";
 import React, { useEffect, useState } from "react";
 import CreateCommentReply from "./CreateCommentReply";
 import { getCommentsByParentIdAction } from "@/actions/comment";
@@ -24,6 +24,7 @@ const CommentReplies = ({
   const [request, setrequest] = useState(1);
   const [data, setData] = useState<Array<FullCommentType>>([]);
   const [hasMore, setHasmore] = useState(true);
+  const [ocult, setOcult] = useState(false);
 
   useEffect(() => {
     if (data.length === comment.responses) {
@@ -31,46 +32,63 @@ const CommentReplies = ({
     }
   }, [data, comment]);
 
+   const handleResponsesLength = async () => {
+    if (ocult && data.length > 0) {
+      setOcult(false);
+      return; 
+    }
+
+    if (hasMore) {
+      const comments = await getCommentsByParentIdAction(
+        comment.id,
+        request
+      );
+      setData((prev) => [...prev, ...comments]);
+      setrequest((prev) => prev + 1);
+    }
+  };
+
   return (
     <div className="flex flex-col overflow-hidden gap-3">
-      <div className="flex items-end gap-1 text-xs ">
-        {comment.responses > 0 && (
-          <div className="flex flex-col gap-3 w-full">
-            <div className="ml-7">
-              {data.map((comment) => (
-                <div key={comment.id} className="w-full">
-                  <Comment
-                    comment={comment}
-                    currentUserIdLog={currentUserIdLog}
-                    commentReply={true}
-                  />
-                </div>
-              ))}
+      {!ocult && (
+        <div className="ml-7">
+          {data.map((comment) => (
+            <div key={comment.id} className="w-full">
+              <Comment
+                comment={comment}
+                currentUserIdLog={currentUserIdLog}
+                commentReply={true}
+              />
             </div>
+          ))}
+        </div>
+      )}
 
-            {hasMore && (
-              <button
-                className=" text-text-gray hover:text-icon-blue "
-                onClick={async () => {
-                  const comments = await getCommentsByParentIdAction(
-                    comment.id,
-                    request
-                  );
-                  setData((prev) => {
-                    return [...prev, ...comments];
-                  });
-                  setrequest((prev) => prev + 1);
-                }}
-              >
-                <div className="flex  gap-1 text-xs cursor-pointer">
-                  <p className=" transition-colors duration-200 ease-in ">
-                    Ver {comment.responses - data.length} respuestas
-                  </p>
-                  <ChevronDown size={20} />
-                </div>
-              </button>
-            )}
-          </div>
+      <div className="flex gap-4 items-center text-text-gray max-w-max text-xs ">
+        {(hasMore || ocult) && (
+          <button
+            onClick={handleResponsesLength}
+          >
+            <div className="flex  gap-1 cursor-pointer hover:text-icon-blue ">
+              <p className="transition-colors duration-200 ease-in">
+                {ocult
+                  ? `Ver ${comment.responses } respuestas`
+                  : `Ver ${comment.responses - data.length} respuestas`} 
+              </p>
+              <ChevronDown size={20} />
+            </div>
+          </button>
+        )}
+
+        {data.length > 0 && !ocult && (
+          <button onClick={() => setOcult(true)}>
+            <div className="flex  gap-1 cursor-pointer hover:text-icon-blue ">
+              <p className=" transition-colors duration-200 ease-in ">
+                Ocultar Todo
+              </p>
+              <ChevronUp size={20} />
+            </div>
+          </button>
         )}
       </div>
 
