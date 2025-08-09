edb3be4f158d977779409573aeba28f3ef034539
Amir Hurtado <a.hurtado@utp.edu.co>
2025-08-09 10:35:18 -0500

    Guardar cambios antes de merge

---
+++ client/src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,122 @@
+"use client";
+
+import * as Clerk from "@clerk/elements/common";
+import * as SignIn from "@clerk/elements/sign-in";
+import { GoogleIcon } from "@/constants";
+import { AtSign, LoaderCircle, Lock } from "lucide-react";
+import Link from "next/link";
+import Separator from "@/components/ui/Separator";
+import Logo from "@/components/ui/Logo";
+
+const SignInPage = () => {
+  return (
+    <div className="h-screen  flex flex-col md:flex-row items-center justify-center gap-8 md:gap-0 md:justify-between p-4 overflow-y-auto overflow-x-hidden  ">
+      <Logo />
+
+      <div className="w-full flex flex-col gap-8 md:gap-13 justify-center items-center md:items-start">
+        <div className="flex flex-col gap-3 md:gap-5 items-center md:items-start ">
+          <p className="font-poppins text-md md:text-3xl font-bold">
+            Desarrollado por Amir Hurtado.
+          </p>
+          <p className="text-sm md:text-xl font-semibold">
+            Únete a la experiencia{" "}
+            <span className="text-icon-green font-bold ">ALAPP.</span>
+          </p>
+        </div>
+
+        <SignIn.Root>
+          <div className="flex flex-col gap-5 w-full ">
+            <SignIn.Step
+              name="start"
+              className="flex flex-col gap-5 items-center md:items-start"
+            >
+              <Clerk.Connection
+                name="google"
+                className="md:w-full flex items-center justify-center md:justify-start"
+              >
+                <div className="max-w-min md:max-w-max cursor-pointer flex items-center gap-5 py-3 md:py-3 px-3 md:px-11 rounded-xl border-1 border-white  hover:bg-white hover:text-black transition-all duration-300 ease-in">
+                  <GoogleIcon />
+                  <p className="hidden md:block font-semibold text-sm md:text-lg">
+                    Inicia sesion con google
+                  </p>
+                </div>
+              </Clerk.Connection>
+
+              <Separator />
+
+              <div className="flex flex-col gap-6  md:gap-7 w-full  md:w-[21rem]">
+                <Clerk.Field
+                  name="identifier"
+                  className="w-full md:w-[21rem] md:p-0 "
+                >
+
+
+                  <div className="relative w-full">
+                    <AtSign
+                    strokeWidth={1}
+                    size={20}
+                    className="absolute left-3 top-1/2 -translate-y-1/2 text-icon-green "
+                  />
+                  <Clerk.Input
+                    className="border-1 border-border py-2 md:py-3 pl-12 rounded-xl bg-input w-full placeholder:font-poppins placeholder:text-sm "
+                    placeholder="Email o nombre de usuario"
+                  />
+
+                  </div>
+                  
+                  <Clerk.FieldError  className="text-red-500 text-xs mt-1" />
+                </Clerk.Field>
+
+
+                <Clerk.Field
+                  name="password"
+                  className=" w-full md:w-[21rem]  md:p-0 "
+                >
+
+                  <div className="relative w-full">
+                    <Lock
+                    strokeWidth={1}
+                    size={20}
+                    className="absolute left-3 top-1/2 -translate-y-1/2 text-icon-green"
+                  />
+                  <Clerk.Input
+                    className="border-1 border-border py-2 md:py-3 pl-12 rounded-xl bg-input w-full placeholder:font-poppins placeholder:text-sm "
+                    placeholder="Contraseña "
+                  />
+
+                  </div>
+                  
+                  <Clerk.FieldError className="text-red-500 text-xs mt-1" />
+                </Clerk.Field>
+              </div>
+
+              <SignIn.Action
+                className=" w-full  cursor-pointer md:w-[21rem] mt-4 rounded-xl   bg-icon-green font-bold py-2  md:py-4  hover:bg-icon-blue transition-all duration-300 ease-in"
+                submit
+              >
+                <Clerk.Loading>
+                  {(isLoading) =>
+                    isLoading ? (
+                      <LoaderCircle className="animate-spin w-full" />
+                    ) : (
+                      "Inicia sesión"
+                    )
+                  }
+                </Clerk.Loading>
+              </SignIn.Action>
+            </SignIn.Step>
+          </div>
+        </SignIn.Root>
+
+        <p className="text-sm">
+          ¿Primera vez?{" "}
+          <span className="underline text-icon-blue">
+            <Link aria-label="ir a crear cuenta" href={"/sign-up"}>Crea una cuenta</Link>
+          </span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default SignInPage;
---
+++ client/src/features/post/components/Comments/CommentReplies/ReplyControls.tsx
@@ -0,0 +1,46 @@
+
+import { ChevronDown, ChevronUp } from "lucide-react";
+
+
+interface ReplyControlsProps {
+    hasMore: boolean
+    ocult: boolean
+    handleOcult: () => void
+    showResponses: () => void
+
+    commentReponses: number
+    dataLength: number
+}
+
+
+const ReplyControls = ({hasMore, ocult, handleOcult, showResponses, commentReponses, dataLength} : ReplyControlsProps) => {
+  return (
+    <div className="flex gap-4 items-center text-text-gray max-w-max text-xs ">
+        {(hasMore || ocult) && (
+          <button onClick={showResponses}>
+            <div className="flex  gap-1 cursor-pointer hover:text-icon-blue ">
+              <p className="transition-colors duration-200 ease-in">
+                {ocult
+                  ? `Ver ${commentReponses} respuestas`
+                  : `Ver ${commentReponses - dataLength} respuestas`}
+              </p>
+              <ChevronDown size={20} />
+            </div>
+          </button>
+        )}
+
+        {dataLength > 0 && !ocult && (
+          <button onClick={handleOcult}>
+            <div className="flex  gap-1 cursor-pointer hover:text-icon-blue ">
+              <p className=" transition-colors duration-200 ease-in ">
+                Ocultar Todo
+              </p>
+              <ChevronUp size={20} />
+            </div>
+          </button>
+        )}
+      </div>
+  )
+}
+
+export default ReplyControls
