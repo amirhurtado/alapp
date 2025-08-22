5749a1a445657f314083f2b6de72be7b1914e0d9
Amir Hurtado <a.hurtado@utp.edu.co>
2025-08-21 23:58:34 -0500

    Implementation of improvement to not allow creating an event if you are not the admin

--- client/src/features/groups/components/InfoGroup/CreateEventButton.tsx
+++ client/src/features/groups/components/InfoGroup/CreateEventButton.tsx
@@ -3,9 +3,11 @@ import React from 'react'
 
 const CreateEventButton = ({disabled}: {disabled: boolean}) => {
   return (
-    <div className={`bg-icon-green cursor-pointer ${disabled && "opacity-[0.8]"} text-black text-center rounded-lg w-full px-2 py-2  xxl:h-auto flex items-center gap-3`}>
+    <div className={`bg-icon-green cursor-pointer ${disabled && "opacity-[0.2]"} text-black text-center rounded-lg w-full px-2 py-2  xxl:h-auto flex items-center gap-2 md:gap-3 md:top-0 top-[-1rem] relative`}>
           <Plus size={19} />
           <p className="hidden xxl:block ">Crear evento</p>
+          <p className=" xxl:hidden text-sm">Evento</p>
+
     </div>
   )
 }
