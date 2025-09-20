0a47e465fbef13b125a1acac609fd8269e24aeea
Amir Hurtado <a.hurtado@utp.edu.co>
2025-09-19 23:39:14 -0500

    Implementation of recommendations in the search section when there is no query V2

--- client/src/features/follows/FollowButton.tsx
+++ client/src/features/follows/FollowButton.tsx
@@ -1,6 +1,17 @@
 "use client";
 
 import { Trash, UserRoundCheck } from "lucide-react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 
 interface FollowButtonProps {
   isFriend: boolean | undefined;
@@ -23,13 +34,31 @@ const FollowButton = ({
             </div>
           )}
 
-          <button
-            onClick={onFollow}
-            className="flex gap-2 cursor-pointer  bg-red-500 items-center justify-center rounded-lg px-2 py-1 active:scale-[0.95] hover:bg-red-400 transition-all duration-200 ease-in"
-          >
-            <Trash size={20} />
-            {fromProfile && <p className="text-xs">Dejar de seguir</p>}
-          </button>
+          <AlertDialog>
+            <AlertDialogTrigger asChild>
+              <button
+                className="flex gap-2 cursor-pointer  bg-red-500 items-center justify-center rounded-lg px-2 py-1 active:scale-[0.95] hover:bg-red-400 transition-all duration-200 ease-in"
+              >
+                <Trash size={20} />
+                {fromProfile && <p className="text-xs">Dejar de seguir</p>}
+              </button>
+            </AlertDialogTrigger>
+
+            <AlertDialogContent>
+              <AlertDialogHeader>
+                <AlertDialogTitle>¿Estás seguro?</AlertDialogTitle>
+                <AlertDialogDescription>
+                  Esta acción dejará de seguir a este usuario. ¿Quieres continuar?
+                </AlertDialogDescription>
+              </AlertDialogHeader>
+              <AlertDialogFooter>
+                <AlertDialogCancel>Cancelar</AlertDialogCancel>
+                <AlertDialogAction onClick={onFollow}>
+                  Continuar
+                </AlertDialogAction>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
         </div>
       ) : (
         <div className="text-black hover:text-white px-3 rounded-lg h-8 flex items-center cursor-pointer bg-primary-color active:scale-[0.95] transition-all duration-200 ease-in">
