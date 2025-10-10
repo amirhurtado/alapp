d2e185aa2b5fbc6644a14212d1114bada44f9e9b
Amir Hurtado <a.hurtado@utp.edu.co>
2025-10-10 18:02:41 -0500

    FIx 404 v1

--- client/src/app/(main)/page.tsx
+++ client/src/app/(main)/page.tsx
@@ -1,23 +1,29 @@
 "use server";
-
 import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation"; 
 import { userExistsAction } from "@/actions/user/createUser";
 import { getPostsAction } from "@/actions/post/getPost";
 import FeedSection from "@/features/feed/components/FeedSection";
 
 export default async function Home() {
   const currUser = await currentUser();
-
-  if (!currUser) return;
-
+  
+  if (!currUser) {
+    redirect('/sign-in'); // 👈 Redirige en vez de return vacío
+  }
+  
   const [userData, posts] = await Promise.all([
     userExistsAction(currUser),
     getPostsAction(currUser.id, "mainFeed"),
   ]);
+
   return (
     <div className="h-screen flex flex-col overflow-hidden">
-
-      <FeedSection posts={posts} currentUser={{id: userData.id, imgUrl: userData.imageUrl}} placement="mainFeed"  />
+      <FeedSection 
+        posts={posts} 
+        currentUser={{id: userData.id, imgUrl: userData.imageUrl}} 
+        placement="mainFeed" 
+      />
     </div>
   );
 }
