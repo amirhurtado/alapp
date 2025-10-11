ca9b2b43289862b0f37c18ebdaf0bfd4065fcdf1
Amir Hurtado <a.hurtado@utp.edu.co>
2025-10-11 11:37:48 -0500

    Correction in invalidate query of delete comment v2

--- client/src/features/post/components/FullPostView.tsx
+++ client/src/features/post/components/FullPostView.tsx
@@ -4,6 +4,7 @@ import { FullCommentType, FullPostType } from "@/types";
 import CommentSection from "@/features/post/components/Comments/CommentSection";
 import PostCard from "@/features/post/components/PostCard";
 import { useQuery } from "@tanstack/react-query";
+import { getPostByIdAction } from "@/actions/post/getPost";
 
 interface FullPostView {
   currentUser: {
@@ -15,28 +16,39 @@ interface FullPostView {
   comments: FullCommentType[];
 }
 
+
+
+
 const FullPostView = ({
   currentUser,
   post: originalPost,
   comments,
 }: FullPostView) => {
-  const queryKey = ["post" , {id: originalPost.id}];
+  const queryKey = ["post", { id: originalPost.id }];
 
-  const { data } = useQuery({
+  const { data, isLoading } = useQuery({
     queryKey,
-    queryFn: () => originalPost,
+    queryFn: () => getPostByIdAction(originalPost.id),
+
     initialData: originalPost,
-    enabled: false
+    
   });
+  
+  if (isLoading && !data) {
+      return <div>Cargando...</div>
+  }
+
+  if (!data) {
+      return <div>No se encontró el post.</div>
+  }
 
   return (
     <>
       <PostCard
-        post={data}
+        post={data!}
         currentUserId={currentUser.id}
         queryKey={queryKey}
         fromPostInfo={true}
-
       />
       <CommentSection
         comments={comments}
